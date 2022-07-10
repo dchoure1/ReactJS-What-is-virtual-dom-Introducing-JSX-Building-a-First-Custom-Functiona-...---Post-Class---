7c6696ec7ef13d0a36320cf6b6ce83df7e3c2c9c
dchoure1 <dchoure1@gmail.com>
2022-07-10 23:01:36 +0530

    copy

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Para from "./Para";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <Para/>
+    </div>
   )
 }
 
